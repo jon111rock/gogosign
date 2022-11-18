cf8f2c32d8dcc75035313521b331d02babb97ee2
yu teng hsueh <jon111rock@gmail.com>
2022-11-18 18:48:59 +0800

    chore: setup react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import route from '@/router';
+
+const router = createBrowserRouter(route.routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 );
---
+++ src/router.jsx
@@ -0,0 +1,15 @@
+import App from '@/App';
+import Manager from '@/pages/Manager';
+
+export default {
+	routes: [
+		{
+			path: '/',
+			element: <App />,
+		},
+		{
+			path: '/manager',
+			element: <Manager />,
+		},
+	],
+};
