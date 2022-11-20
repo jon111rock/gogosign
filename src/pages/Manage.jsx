366ffdf9ae9afe5988d26c6a04c222d9f48f7416
yu teng hsueh <jon111rock@gmail.com>
2022-11-20 14:16:36 +0800

    format

--- src/pages/Manage.jsx
+++ src/pages/Manage.jsx
@@ -1,9 +1,11 @@
 import logoImage from '@/assets/gogosign-log.png';
 
 const Manage = () => {
-	return <div>
-		<img src={logoImage}/>
-	</div>;
+	return (
+		<div>
+			<img src={logoImage} />
+		</div>
+	);
 };
 
 export default Manage;
