0865ceab23e0b09ee9a210188057437541d24daf
yu teng hsueh <jon111rock@gmail.com>
2022-11-21 23:19:15 +0800

    feat: ManageFileCard props

--- src/components/ManageFileCard.jsx
+++ src/components/ManageFileCard.jsx
@@ -1,23 +1,32 @@
-import Icon from "./Icon"
+import PropTypes from 'prop-types';
+import Icon from './Icon';
 
-const ManageFileCard = () => {
-  return (
-    <div className='flex flex-col gap-7 p-5 rounded-xl  bg-white shadow-md'>
-    <span>文件01</span>
-    <div className='flex justify-between'>
-      <div>
-        <span className='mr-14'>2022/11/11 23:15:21</span>
-        <span>100KB</span>
-      </div>
-      <div className='flex items-center gap-2'>
-        <Icon type='download' />
-        <Icon type='share' />
-        <Icon type='edit' />
-        <Icon type='cross' />
-      </div>
-    </div>
-  </div>
-  )
-}
+const ManageFileCard = ({ item = {} }) => {
+	return (
+		<div className='flex flex-col gap-7 p-5 rounded-xl  bg-white shadow-md'>
+			<span>{item.name}</span>
+			<div className='flex justify-between'>
+				<div>
+					<span className='mr-14'>{item.creatAt}</span>
+					<span>{item.fileSize}</span>
+				</div>
+				<div className='flex items-center gap-2'>
+					<Icon type='download' />
+					<Icon type='share' />
+					<Icon type='edit' />
+					<Icon type='cross' />
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default ManageFileCard
+ManageFileCard.propTypes = {
+	item: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		creatAt: PropTypes.string.isRequired,
+		fileSize: PropTypes.string.isRequired,
+	}).isRequired,
+};
+
+export default ManageFileCard;
