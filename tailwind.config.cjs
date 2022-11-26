9348cb686d7caa2168424123a641ec179748c34d
Louise <huei.yuan1996@gmail.com>
2022-11-26 20:26:08 +0800

    fix(tailwind): change color variable 'grey' to 'gray' and add white color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
+				white: '#FFFFFF',
 				'yellow-100': '#FDF5DD',
 				'yellow-200': '#FECD6D',
 				'yellow-300': '#F9B867',
@@ -11,14 +12,14 @@ module.exports = {
 				'yellow-500': '#EDBA56',
 				'yellow-600': '#E09941',
 				'yellow-700': '#DCA354',
-				'grey-100': '#F2F2F2',
-				'grey-200': '#DDDDDD',
-				'grey-300': '#CCCCCC',
-				'grey-400': '#A6A6A6',
-				'grey-500': '#757575',
-				'grey-600': '#757267',
-				'grey-600': '#6C757D',
-				'grey-800': '#54595E',
+				'gray-100': '#F2F2F2',
+				'gray-200': '#DDDDDD',
+				'gray-300': '#CCCCCC',
+				'gray-400': '#A6A6A6',
+				'gray-500': '#757575',
+				'gray-600': '#757267',
+				'gray-600': '#6C757D',
+				'gray-800': '#54595E',
 				'black-500': '#231B00'
 			},
 		},
